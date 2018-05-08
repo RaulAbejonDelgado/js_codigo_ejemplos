e34e7a715809cb209c1a64bb2095f1e32514455e
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-05-08 17:53:45 +0200

    Switch
    Condicional Switch

---
+++ switch.js
@@ -0,0 +1,44 @@
+//switch para cuando tenemos multiples condiciones
+//Si mes no coincidice con ninguno de los case toma el valor default
+var mes =3;
+switch(mes){
+	case 1:
+		console.log('Enero');
+		break;
+	case 2:
+		console.log('febrero');
+		break;
+	case 3:
+		console.log('marzo');
+		break;
+	case 4:
+		console.log('abril');
+		break;
+	case 5:
+		console.log('mayo');
+		break;
+	case 6:
+		console.log('junio');
+		break;
+	case 7:
+		console.log('julio');
+		break;
+	case 8:
+		console.log('agosto');
+		break;
+	case 9:
+		console.log('septiembre');
+		break;
+	case 10:
+		console.log('octubre');
+		break;
+	case 11:
+		console.log('noviembre');
+		break;
+	case 12:
+		console.log('diciembre');
+		break;
+	default:
+		console.log('Por defecto');
+
+}
