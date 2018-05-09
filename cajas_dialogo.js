e19e7c5349ece98a475ad071ec2acf1adeb7ba02
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-05-10 00:54:36 +0200

    cajas de dialo
    Tipos de cajas de dialogo

---
+++ cajas_dialogo.js
@@ -0,0 +1,9 @@
+//cajas de dialogos
+//caja informativa
+alert('Hey Rul');
+//caja donde hay posibilidad de pulsar sobre aceptar o cancelar
+var acepto = confirm('Seguro de su accion');
+console.log(acepto);
+//captura de mensaje por pantalla
+var infoCliente = prompt('ingrese su nombre','nombre');
+console.log(infoCliente);
